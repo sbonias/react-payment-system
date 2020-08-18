81474adf13868b7bd4def1c28c73f44dc12a8ace
Sam Bonias <sbonias84@gmail.com>
2020-08-18 18:17:22 -0400

    Updates

---
+++ precache-manifest.af92f17ae8264a842eae72108d629ada.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726a148204ecf43cfe719939066ddba8",
+    "url": "/react-payment-system/index.html"
+  },
+  {
+    "revision": "fc14c3bacd3234df18d3",
+    "url": "/react-payment-system/static/css/2.61db2540.chunk.css"
+  },
+  {
+    "revision": "e1247ca17e0f069fd969",
+    "url": "/react-payment-system/static/css/main.6bdf5fc1.chunk.css"
+  },
+  {
+    "revision": "fc14c3bacd3234df18d3",
+    "url": "/react-payment-system/static/js/2.951ee6d8.chunk.js"
+  },
+  {
+    "revision": "33c02c42ab7314be00ddaa44dbd96c39",
+    "url": "/react-payment-system/static/js/2.951ee6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1247ca17e0f069fd969",
+    "url": "/react-payment-system/static/js/main.6b38b2db.chunk.js"
+  },
+  {
+    "revision": "681d6184cf3fb56b094c",
+    "url": "/react-payment-system/static/js/runtime-main.52a68863.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/react-payment-system/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/react-payment-system/static/media/arrow_right.8ef6a08c.svg"
+  }
+]);
